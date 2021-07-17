2abc8c0eacd9ec85704029ce053d8417b04be835
Aleks24dan <aleks24dan@gmail.com>
2021-07-17 14:00:21 +0200

    1

---
+++ src/js/fetchCountries.js
@@ -0,0 +1,17 @@
+/*
+Напиши функцию fetchCountries(name) которая делает HTTP-запрос и возвращает промис с массивом стран 
+- результатом запроса. Вынеси её в отдельный файл fetchCountries.js и сделай именованный экспорт.
+*/
+
+import '../css/styles.css';
+
+
+const BASE_URL = 'https://restcountries.eu/rest/v2';
+
+function fetchCountry(countryName) {
+    return fetch(`${BASE_URL}/name/${countryName}`).then(response => 
+            response.json(),
+        );        
+}
+
+export default { fetchCountry };
